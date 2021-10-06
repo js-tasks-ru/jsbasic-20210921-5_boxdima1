903aae5a6383d632058b0da4f99d43e59ca722c3
Dmitry <boxdima1@gmail.com>
2021-10-06 19:43:54 -0400

    lesson 5 is done

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,9 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let rows = table.rows;
+  let len = rows.length - 1;
+  for (let i = 0; i < len + 1; i++) {
+    let cell = rows[i].cells;
+    cell[i].style.backgroundColor = 'red';
+    cell[len - i].style.backgroundColor = 'red';
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,21 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.getElementsByTagName('tbody')[0].rows;
+  for (let i = 0; i < rows.length; i++) {
+    let tr = rows[i];
+    if (+tr.cells[1].innerText < 18) {
+      tr.style.textDecoration = 'line-through';
+    }
+    if (tr.cells[2].innerText === 'f') {
+      tr.classList.add('female');
+    } else if (tr.cells[2].innerText === 'm') {
+      tr.classList.add('male');
+    }
+    if (!tr.cells[3].hasAttribute('data-available')) {
+      tr.hidden = true;
+    } else if (tr.cells[3].dataset.available === 'true') {
+      tr.classList.add('available');
+    } else {
+      tr.classList.add('unavailable');
+    }
+  }
 }
