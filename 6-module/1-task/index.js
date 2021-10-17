d6275e878e03c682d837e79f0b6877048a3bf623
Dmitry <boxdima1@gmail.com>
2021-10-17 10:55:33 -0400

    lesson 7 1 task did not pass the tests

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,34 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = document.createElement('table');
+    this.render();
+  }
+
+  render() {
+    for (let obj of this.rows) {
+      const row = this.elem.insertRow();
+      for (const value of Object.values(obj)) {
+        const cell = row.insertCell();
+        cell.innerText = value;
+      }
+      this.createBtn(row);
+    }
+    const thead = document.querySelector('thead');
+    this.elem.insertAdjacentElement('afterbegin', thead);
+  }
+
+  createBtn(row) {
+    const btn = document.createElement('button');
+    btn.addEventListener('click', this.deleteRow);
+    btn.innerText = 'X';
+    const btnCell = row.insertCell();
+    btnCell.append(btn);
+  }
+
+  deleteRow(event) {
+    const selectedElem = event.target.parentNode.parentNode;
+    selectedElem.remove();
   }
 }
